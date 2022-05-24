cbb352ba7976a875bb62c1ddd3c92df822675549
git-BR <bhrochamail@gmail.com>
2022-05-24 17:51:44 -0300

    Bootstraping setup

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use sqlx::postgres::PgPool;
+use std::net::TcpListener;
+use update_delivery::configuration::get_configuration;
+use update_deliverya::startup::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
+}
